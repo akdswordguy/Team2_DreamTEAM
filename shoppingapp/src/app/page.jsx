2d7ee2b881a29e90cca7f98f13d907fed963099d
Kurt Sony <kurtsony16@gmail.com>
2025-02-19 23:07:03 +0530

    Minor corrections in code

--- shoppingapp/src/app/page.jsx
+++ shoppingapp/src/app/page.jsx
@@ -23,7 +23,7 @@ const LandingPage = () => {
         </div>
         <ul className="nav-links">
           <li><Link href="#">Home</Link></li>
-          <li><Link href="#">Shop</Link></li>
+          <li><Link href="/Shop">Shop</Link></li>
           <li><Link href="#">About</Link></li>
           <li><Link href="#">Contact</Link></li>
         </ul>
