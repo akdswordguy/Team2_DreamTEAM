65125dc434a3ca16de6422c0ad98714388f006d0
Prarthana Desai <137315057+gitpraths@users.noreply.github.com>
2025-03-05 15:31:56 +0530

    Removed old backend files

--- backend_store_online/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("auth_app/", include("auth_app.urls")),
-    path("product/", include("product.urls")),
-]
--- product/schema.py
+++
@@ -1,5 +0,0 @@
-import strawberry
-from product.queries import Query
-from product.mutations import ProductMutations
-
-schema = strawberry.Schema(query=Query, mutation=ProductMutations)
