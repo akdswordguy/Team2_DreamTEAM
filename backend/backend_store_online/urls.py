1cb6ac744739a7195a76bec653675b8217aac7e1
Prarthana Desai <137315057+gitpraths@users.noreply.github.com>
2025-04-13 02:24:35 +0530

    minor change in urls.py (backend_store_online)

--- backend/backend_store_online/urls.py
+++ backend/backend_store_online/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
+from strawberry.django.views import GraphQLView
+from product.schema import schema as product_schema  # Correct path to your schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth_app/", include("auth_app.urls")),
     path("product/", include("product.urls")),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(schema=product_schema))),  # Exposing GraphQL
 ]
-
