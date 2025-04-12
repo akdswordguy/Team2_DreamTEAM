b394a4487e0819b0447be6db0d8a6a746de7bfd9
Prarthana Desai <137315057+gitpraths@users.noreply.github.com>
2025-04-13 02:11:10 +0530

    minor change

--- backend/backend_store_online/settings.py
+++ backend/backend_store_online/settings.py
@@ -102,7 +102,7 @@ JAZZMIN_SETTINGS = {
 
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware"
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
